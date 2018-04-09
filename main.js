a577bfb3a0da8cec6d6ce105572b14ec181e64a2
Felipe Martins <felipemartins81@gmail.com>
2018-04-09 18:25:22 -0300

    splitting in functions

--- main.js
+++ main.js
@@ -12,73 +12,82 @@ var deploys   = portal + 'job/Deploy/';
 var deployUrl      = deploys + 'job/';
 var deployHmlList  = deployUrl + 'Homologa%C3%A7%C3%A3o/';
 var deployProdList = deployUrl + 'Prod/';
+var iconYellowBall = '<img src="/static/d38f3056/images/32x32/yellow.png">';
 
-// less opacity for non favorites
-switch (window.location.pathname) {
-  case portal:
-  case frontV1:
-  case frontV2:
-  case frontV3:
-  case deploys:
-  case deployHmlList:
-  case deployProdList:
-    jQuery('.icon-fav-inactive').parents('tr').css('opacity','0.2');
-    return;
+function setGridFavoritesOpacity() { 
+  switch (window.location.pathname) {
+    case portal:
+    case frontV1:
+    case frontV2:
+    case frontV3:
+    case deploys:
+    case deployHmlList:
+    case deployProdList:
+      jQuery('.icon-fav-inactive').parents('tr').css('opacity','0.2');
+      return;
+  }
 }
 
-// check deploy urls
-if (window.location.search.includes('tag')) {
-  var pos = window.location.search.indexOf('tag');
-  var tag = window.location.search.substring(pos + 4);
-  jQuery('[value=tag]').next('input').val(tag);
-  console.warn(window.location.search.substring(pos + 4));
-}
-
-// check build urls
-switch (window.location.pathname) {
-  case releaseV1:
-    deployUrl += 'Homologa%C3%A7%C3%A3o/job/Frontend/build?delay=0sec';
-    break;
-  case releaseV2:
-    deployUrl += 'Homologa%C3%A7%C3%A3o/job/Mktp-Front/build?delay=0sec';
-    break;
-  case releaseV3:
-    deployUrl += 'Homologa%C3%A7%C3%A3o/job/mktp-front-ng/build?delay=0sec';
-    break;
-  default:
-    return;
-}
-
-
-// wait 10s
-setTimeout(function() { 
-  // loop each 2s
-  var interval = setInterval(function() { 
-    console.log('custom script: checking build status...');
+function makeLinkToDeploy() { 
+  jQuery('#linkToDeploy').remove();
     
-    // check build succes class
-    // if ( jQuery('.time-remaining')[0] !== undefined  ) { return }
-    if ( !jQuery('.job .stage-cell-3').first().hasClass('SUCCESS') && jQuery('.time-remaining')[0] !== undefined  ) { return }
-    
-    console.log('custom script: yep, build finished!..');
-    
-    // get last build name
-    var lastBuild = jQuery('.icon-blue').first().parents('.build-icon').next().text();
+  if (jQuery('.progress-bar')[0] !== undefined) { return }
+  
+  var deployUrl = deploys + 'job/';
+  var lastBuild = '';
+  var server = '';
+  var environment = '';
+  
+  switch (window.location.pathname) {
+    case releaseV1:
+      server = 'Homologa%C3%A7%C3%A3o';
+      environment = 'Frontend';
+      break;
+    case releaseV2:
+      server = 'Homologa%C3%A7%C3%A3o';
+      environment = 'Mktp-Front';
+      break;
+    case releaseV3:
+      server = 'Homologa%C3%A7%C3%A3o';
+      environment = 'mktp-front-ng';
+      break;
+    case masterV1:
+      server = 'Prod';
+      environment = 'Frontend';
+      break;
+    case masterV2:
+      server = 'Prod';
+      environment = 'Mktp-Front';
+      break;
+    case masterV3:
+      server = 'Prod';
+      environment = 'mktp-front-ng';
+      break;
+  }
+  
+  lastBuild = jQuery('.icon-blue').first().parents('.build-icon').next().text();
+  deployUrl += server +'/job/'+ environment +'/build?delay=0sec';
+  
+  if (server === 'Homologa%C3%A7%C3%A3o') {
     deployUrl += '&tag=' + lastBuild; 
-    console.table({ 'lastBuild': lastBuild });
-    
-    // append link
-    jQuery('#pipeline-box h2').append('<a href="'+ deployUrl +'" style="margin: 0 0 0 200px;"> <img src="/static/d38f3056/images/32x32/yellow.png"> Fazer Deploy em Hml &raquo; <i id="lastBuild"></i> </a>');
-    
-    jQuery('#lastBuild').append( lastBuild );
-    
-    // clear interval
-    setTimeout(function(){
-      if ( jQuery('#lastBuild').text() !== "" ) {
-        console.log('custom script: done, stoping loop...');
-        clearInterval( interval );
-      }
-    }, 500);
-    
-  }, 2000);
-}, 10000);
+  }
+  
+  jQuery('#pipeline-box h2').append('<a id="linkToDeploy" href="'+ deployUrl +'" style="margin: 0 0 0 200px;"> '+ iconYellowBall +' Fazer Deploy em '+ (server === 'Prod' ? 'Prod' : 'Homol') +' &raquo; '+ lastBuild +' </a>');
+}
+
+function getUrlQueryParam() {
+  if (window.location.search.includes('tag')) {
+    var pos = window.location.search.indexOf('tag');
+    var tag = window.location.search.substring(pos + 4);
+    jQuery('[value=tag]').next('input').val(tag);
+  }
+}
+
+// init...
+
+setGridFavoritesOpacity();
+getUrlQueryParam();
+
+setInterval(function() {
+  makeLinkToDeploy();
+}, 2000);
